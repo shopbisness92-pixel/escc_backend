6002e1d074b20d14d037d56dee5eeedc6cd87360
ESCC <ESCC@gmail.com>
2026-01-21 23:42:39 +0500

    Prepare backend for Render/Fly.io deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn escc_backend.wsgi --log-file -
--- escc_backend/wsgi.py
+++ escc_backend/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for escc_backend project.
 
@@ -18,24 +18,4 @@ application = get_wsgi_application()
 
 app = application
 
-=======
-"""
-WSGI config for escc_backend project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escc_backend.settings')
-
-application = get_wsgi_application()
-
-app = application
-
->>>>>>> db10e5ef20a7e0293aa4275ab1c6357019f9d8ee
